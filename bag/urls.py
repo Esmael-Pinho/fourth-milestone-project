97c43631578bbff6a9d89b1efb930ebb4efc12df
Esmael Pinho <esma.pinho@gmail.com>
2024-02-21 06:47:49 +0000

    added add to bag funcionality

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,6 @@ from .views import apply_promo_code_view
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('apply-promo-code/', apply_promo_code_view, name='apply_promo_code'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -23,5 +23,26 @@ def apply_promo_code_view(request):
     return redirect(reverse(''))
 
 def is_valid_promo_code(promo_code):
-    # Implement your logic to validate the promo code (e.g., check against a list of valid codes)
     return promo_code in ['HALFOFF', 'MINIM2024']
+
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
