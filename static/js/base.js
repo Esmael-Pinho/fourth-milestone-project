687ab64cae391260a51037ffb645ef90acd1ea34
Esmael Pinho <esma.pinho@gmail.com>
2024-04-26 08:53:15 +0000

    updated time

--- static/js/base.js
+++ static/js/base.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Mar 31, 2024 16:00:00").getTime();
+    var countDownDate = new Date("Mar 31, 2075 16:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
